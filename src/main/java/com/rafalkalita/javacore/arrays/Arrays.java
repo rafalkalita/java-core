976e1b83908724c953cf618894d4a5a9498e4ad6
rafalkalita <rafal@rafalkalita.com>
2014-02-03 16:28:46 +0000

    Arrays - comparing.

--- src/main/java/com/rafalkalita/javacore/arrays/Arrays.java
+++ src/main/java/com/rafalkalita/javacore/arrays/Arrays.java
@@ -92,10 +92,12 @@ public class Arrays {
             number = number * number;
             System.out.println(number);
         }
+        System.out.println();
         for(int j = 0; j < ages.length; j++) {
             System.out.println(ages[j]);
             ages[j] = ages[j] + 100;
         }
+        System.out.println();
         for(int j = 0; j < ages.length; j++) {
             System.out.println(ages[j]);
         }
@@ -116,6 +118,74 @@ public class Arrays {
         }
     }
 
+    public void comparingArrays() {
+
+        System.out.println("\nComparingArrays");
+        int arr1[];
+        int arr2[];
+        arr1 = new int[5];
+        arr2 = new int[5];
+
+        for(int i = 0; i < 5; i++) {
+            arr1[i] = 0;
+            arr2[i] = 0;
+        }
+
+        // Element-by-element comparison
+        // Using indexes is an error prone and tedious approach.
+        boolean areEqual = true;
+        for (int i = 0; i < 5; i++) {
+            if(arr1[i]!= arr2[i]) {
+                areEqual = false;
+            }
+        }
+        System.out.println(areEqual); // Displays true
+
+        // Using the equality operator
+        System.out.println(arr1 == arr2);  // Displays false
+
+        // Using the equals method
+        System.out.println(arr1.equals(arr2));  // Displays false
+
+        // Using the deepEquals method
+        System.out.println(java.util.Arrays.equals(arr1,arr2));
+
+        System.out.println();
+        // The deepEquals method requires an array of objects
+        Person[] people1 = {new Person("John"), new Person("Rafal")};
+        Person[] people2 = {new Person("John"), new Person("Rafal")};
+
+        System.out.println(people1 == people2);
+        System.out.println(people1.equals(people2));
+        System.out.println(java.util.Arrays.equals(people1, people2));
+        System.out.println(java.util.Arrays.deepEquals(people1, people2));
+
+        System.out.println();
+        // Arrays.equals doesn't work for array of int. deepEquals does.
+        int grades1[][] = new int[ROWS][];
+        int grades2[][] = new int[ROWS][];
+
+        grades(grades1);
+        grades(grades2);
+
+        System.out.println(grades1 == grades2);
+        System.out.println(grades1.equals(grades2));
+        System.out.println(java.util.Arrays.equals(grades1, grades2));
+        System.out.println(java.util.Arrays.deepEquals(grades1, grades2));
+    }
+
+    private void grades(int[][] grades) {
+
+        grades[0] = new int[COLS];
+        grades[1] = new int[COLS];
+        grades[0][0] = 0;
+        grades[0][1] = 1;
+        grades[0][2] = 2;
+        grades[1][0] = 3;
+        grades[1][1] = 4;
+        grades[1][2] = 5;
+    }
+
     class Person {
 
         String name;
@@ -133,6 +203,23 @@ public class Arrays {
         @Override
         public String toString() {
             return "Person{name='" + name + '\'' + '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person)) return false;
+
+            Person person = (Person) o;
+
+            if (name != null ? !name.equals(person.name) : person.name != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return name != null ? name.hashCode() : 0;
         }
     }
 }
--- src/main/java/com/rafalkalita/javacore/arrays/ArraysDriver.java
+++ src/main/java/com/rafalkalita/javacore/arrays/ArraysDriver.java
@@ -13,5 +13,6 @@ public class ArraysDriver {
         arrays.declaration();
         arrays.multidimensional();
         arrays.traversing();
+        arrays.comparingArrays();
     }
 }
